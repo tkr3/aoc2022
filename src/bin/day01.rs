e8918c1af97a025b4e889934bd5944d9fa6c6284
tobi1805 <tobi1805@gmx.de>
2022-12-01 14:26:59 +0100

    Solve A1

---
+++ src/bin/day01.rs
@@ -0,0 +1,17 @@
+use std::collections::BTreeSet;
+
+fn main() {
+    let input = include_str!("../../inputs/day01.txt");
+    let mut counts = BTreeSet::new();
+    input.lines().fold(0, |acc, line: &str| {
+        return if line.is_empty() {
+            counts.insert(acc);
+            0
+        } else {
+            acc + line.parse::<i32>().unwrap()
+        }
+    });
+    println!("Max calories: {}", counts.iter().last().unwrap());
+
+    println!("Three most calories: {}", counts.iter().rev().take(3).sum::<i32>());
+}
